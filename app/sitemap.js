ea3c01d51ea207a75ed9cfe2d354b21a97c92388
djoleIT <djordje.petkovic@inovatechit.com>
2026-08-27 01:02:35 +0200

    Delete app directory

--- app/sitemap.js
+++
@@ -1,9 +0,0 @@
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://vratimekuci.rs";
-
-export default function sitemap() {
-  const routes = ["", "/naruci", "/kako-radi", "/galerija", "/faq", "/kontakt", "/privacy", "/terms"];
-  return routes.map((route) => ({
-    url: `${SITE_URL}${route}`,
-    lastModified: new Date(),
-  }));
-}
